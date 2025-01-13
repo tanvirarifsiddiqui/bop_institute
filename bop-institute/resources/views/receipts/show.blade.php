fbcfedcd39e9989b83c64b58ba58ec3a66627618
tanvir <tanvirtmm@gmail.com>
2025-01-13 18:12:09 +0600

    unnecessary files has been deleted

--- bop-institute/resources/views/receipts/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Receipt</h1>
-<p>Formula: {{ $receipt->formula->title }}</p>
-<p>User: {{ $receipt->user->name }}</p>
-<a href="{{ asset('storage/' . $receipt->receipt_pdf) }}" download>Download Receipt</a>
-<a href="{{ asset('storage/' . $receipt->formula->pdf) }}" download>Download Formula</a>
-@endsection
