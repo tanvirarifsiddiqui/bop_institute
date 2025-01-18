fb6543e42f154c27ff4d5782b31d8c93f6a15f98
tanvir <tanvirtmm@gmail.com>
2025-01-18 15:09:50 +0600

    initial structure user end done

---
+++ bop_institute/resources/views/layouts/carousel.blade.php
@@ -0,0 +1,33 @@
+<div id="homeCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('images/carousel_slider/slide1.png') }}" class="d-block w-100" alt="Carousel Image 1">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Welcome to BOP Institute</h5>
+                <p>Your trusted source for chemical formulations.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/carousel_slider/slide1.png') }}" class="d-block w-100" alt="Carousel Image 2">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Explore Our Formula Collection</h5>
+                <p>Get access to verified and tested formulas.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/carousel_slider/slide1.png') }}" class="d-block w-100" alt="Carousel Image 3">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Join Us Today</h5>
+                <p>Sign up to purchase formulas and more.</p>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
---
+++ bop_institute/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-lg-start">
+    <div class="text-center p-3">
+        © {{ date('Y') }} BOP Institute. All Rights Reserved.
+    </div>
+</footer>
